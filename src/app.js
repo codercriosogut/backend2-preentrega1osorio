7fb33f4b7a2082d27ffa105ef93296d7e7031188
tic cristian osorio <cosoriogut@gmail.com>
2024-09-10 16:44:10 -0300

    up 4

--- src/app.js
+++ src/app.js
@@ -3,9 +3,21 @@ import express from 'express';
 import session from 'express-session';
 import mongoose from './config/connect.js';
 import MongoStore from 'connect-mongo';
+import passport from 'passport';
+import sessionsRouter from './routes/sessions.js';
+
+
 
 const app = express()
 const port = 8080
+
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'main',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -19,6 +31,13 @@ app.use(session({
     })
 }))
 
+
+initializePassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/sessions', sessionsRouter);
+app.use('/', viewsRouter);
+
+
 app.listen(port, () => console.log(`Server running on port ${port}`))
-//test
-//test2
